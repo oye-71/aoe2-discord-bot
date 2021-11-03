ab96f39fd8ae994bc4c2672501072f7f478f0579
Etienne DUMONTET <etiennedumontet@hotmail.com>
2021-11-03 23:14:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const functions = require('./functions.js');
+
+// Pour les tests uniquement
+// A voir pour map ça sur une requete de bot discord
+const vanityurl = 'oye71';
+
+(async() => {
+    await functions.getLastMatch(vanityurl);
+})();
