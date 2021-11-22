60a0fe377b05fafc5cf9dca222dab78ac005dac0
oye-71 <etiennedumontet@hotmail.com>
2021-11-22 21:37:32 +0100

    Ajout setup minimal discord

---
+++ discordCommands.js
@@ -0,0 +1,11 @@
+// const { REST } = require('@discordjs/rest');
+// const { Routes } = require('discord-api-types/v9');
+// const { discordClientId, discordGuildId, discordToken } = require('./local.config.json');
+
+// const commands = [];
+
+// const rest = new REST({ version: '9' }).setToken(discordToken);
+
+// rest.put(Routes.applicationGuildCommands(discordClientId, discordGuildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands.'))
+// 	.catch(console.error);
--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const CONFIG = require('./local.config.json') // TODO : Seulement en local, faire ça avec les variables d'environnement
 const functions = require('./functions.js');
 const {Client, Intents} = require('discord.js');
-const client =  new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
+// Nouveau client discord
+const client =  new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
-    console.log("Discord client is ready");
-    client.login(CONFIG.token);
+	console.log('Discord client is ready!');
 });
+
+// Login en tant que bot discord
+client.login(CONFIG.discordToken);
+
+// Commandes acceptées par le bot 
+// const commandes = [
+//     new SlashCommandBuilder().setName('match').setDescription('Renvoie le match en cours d\'un joueur'),
+// 	new SlashCommandBuilder().setName('rank').setDescription('Renvoie les classements d\'un joueur')
+// ];
 
 // TODO IMPLEMENT DISCORD BOT
 (async() => {
     await functions.getLastMatch(CONFIG.vanityUrl);
-
     await functions.getCurrentPlayerInfos(CONFIG.vanityUrl);
 })();
